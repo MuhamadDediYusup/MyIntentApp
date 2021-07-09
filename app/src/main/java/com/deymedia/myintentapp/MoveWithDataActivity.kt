d34e9eb403a730b5bd583b041d1ec3d8abde6f7f
Muhamad Dedi Yusup <mdyusup45@gmail.com>
2021-07-10 01:33:06 +0700

    Initial commit

---
+++ app/src/main/java/com/deymedia/myintentapp/MoveWithDataActivity.kt
@@ -0,0 +1,25 @@
+package com.deymedia.myintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MoveWithDataActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_AGE = "extra_age"
+        const val EXTRA_NAME = "extra_name"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_data)
+
+        val tvDataRecivered: TextView = findViewById(R.id.tv_data_recivered)
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val age = intent.getIntExtra(EXTRA_AGE, 0)
+
+        val text = "Name : $name, \nYour Age : $age"
+        tvDataRecivered.text = text
+    }
+}
